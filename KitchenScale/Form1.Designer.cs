7d710a7de0c2be02a81efea381f4be2572042c88
AKL128 <anthonyluu128@gmail.com>
2023-08-01 00:00:37 -0500

    Added second button for parsing selected file

--- KitchenScale/Form1.Designer.cs
+++ KitchenScale/Form1.Designer.cs
@@ -30,11 +30,10 @@
         {
             openFileDialog1 = new OpenFileDialog();
             button1 = new Button();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
             SuspendLayout();
-            // 
-            // openFileDialog1
-            // 
-            openFileDialog1.FileName = "";
             // 
             // button1
             // 
@@ -46,15 +45,49 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Metric", "Imperial", "Mixed" });
+            comboBox1.Location = new Point(248, 98);
+            comboBox1.MaxDropDownItems = 3;
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 1;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(303, 194);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(485, 233);
+            textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(108, 193);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
 
@@ -63,5 +96,8 @@
 
         private OpenFileDialog openFileDialog1;
         private Button button1;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+        private Button button2;
     }
 }
--- KitchenScale/Form1.cs
+++ KitchenScale/Form1.cs
@@ -6,14 +6,37 @@ namespace KitchenScale
         {
             InitializeComponent();
         }
-
+        private string filePath = "";
         private void button1_Click(object sender, EventArgs e)
         {
             openFileDialog1.Filter = "Text|*.txt|All|*.*";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 MessageBox.Show(openFileDialog1.FileName);
+                filePath = openFileDialog1.FileName;
+                Console.WriteLine(filePath);
             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(filePath);
+
+            string contents = sr.ReadToEnd();
+
+            textBox1.Text = contents;
+
+            sr.Close();
         }
     }
 }
