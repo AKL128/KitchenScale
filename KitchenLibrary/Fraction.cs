4f5b11e99c269041fe5a47974cfd1fd6b5177281
AKL128 <54521114+AKL128@users.noreply.github.com>
2023-08-07 16:47:53 -0500

    Moved Fraction class to library

---
+++ KitchenLibrary/Fraction.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenLibrary
+{
+    public class Fraction
+    {
+        private int numerator;
+        private int denominator;
+        public Fraction(int n, int d)
+        {
+            this.numerator = n;
+            this.denominator = d;
+        }
+        public int getNumerator
+        {
+            get { return this.numerator; }
+            set { this.numerator = value; }
+        }
+
+        public int getDenominator
+        {
+            get { return this.denominator; }
+            set
+            {
+                if (value == 0)
+                {
+                    throw new Exception("0 denominator");
+                }
+
+                this.denominator = value;
+            }
+        }
+
+        public string getString()
+        {
+            if (numerator == 0)
+            {
+                return "";
+            }
+            if (denominator == 1)
+            {
+                return numerator.ToString();
+            }
+            else
+            {
+                return numerator.ToString() + "/" + denominator.ToString();
+            }
+        }
+
+        public static int getGCD(int a, int b)
+        {
+            if (b == 0)
+            {
+                return a;
+            }
+            else
+            {
+                return getGCD(b, a % b);
+            }
+        }
+
+        public Fraction reduce()
+        {
+            int gcd = getGCD(numerator, denominator);
+            return new Fraction(numerator / gcd, denominator / gcd);
+        }
+
+        public Fraction multiply(int constant)
+        {
+            return new Fraction(this.numerator * constant, this.denominator);
+        }
+    }
+}
---
+++ KitchenLibrary/KitchenCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+
+namespace KitchenLibrary
+{
+    public class KitchenCalculator
+    {
+        public string fractionInput { get; set; }
+        public int resizeScale { get; set; }
+
+        public fractionScale
+        public string fractionScale(string input, int scale)
+        {
+            int wholeNumber = 0;
+            string fraction;
+            int numerator;
+            int denominator;
+            string? result = "";
+
+            bool hasWholeNumber = false;
+
+            hasWholeNumber = input.Split(" ").Length > 1;
+
+            fraction = input;
+
+            if (hasWholeNumber == true)
+            {
+                wholeNumber = int.Parse(input.Split(" ")[0]);
+                wholeNumber = wholeNumber * scale;
+                fraction = input.Split(" ")[1];
+            }
+
+            numerator = int.Parse(fraction.Split("/")[0]);
+
+            denominator = int.Parse(fraction.Split("/")[1]);
+
+
+            numerator = numerator * scale;
+
+            Fraction fraction1 = new Fraction(numerator, denominator);
+            if (numerator >= denominator)
+            {
+                wholeNumber += numerator / denominator;
+            }
+
+            int quotient = fraction1.getNumerator / fraction1.getDenominator;
+            Debug.WriteLine("Q: " + quotient);
+            int remainder = fraction1.getNumerator - fraction1.getDenominator * quotient;
+            Debug.WriteLine(fraction1.getNumerator + "-" + fraction1.getDenominator + "*" + quotient);
+            fraction1 = new Fraction(remainder, denominator);
+
+            fraction1 = fraction1.reduce();
+
+            Debug.WriteLine($"{fraction1.getNumerator} / {fraction1.getDenominator}");
+            if (wholeNumber == 0)
+            {
+                return result = fraction1.getString();
+            }
+            else
+            {
+                result = wholeNumber.ToString();
+                result += " " + fraction1.getString();
+                return result;
+            }
+
+        }
+    }
+}
